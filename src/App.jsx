890cf52dcb39a4f4bb781ab90c3d535ecf69cc9e
ToitenDang <tkdang21@gmail.com>
2024-05-01 15:17:07 +0700

    Config MUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <h1>ToitenDang</h1>
+      <Typography variant ="body2" color="text.secondary"> Test Typography</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
