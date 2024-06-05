e05e5e321c93712b0f06d0536014544d67000905
ToitenDang <tkdang21@gmail.com>
2024-06-05 14:37:24 +0700

    Drag Drop Columns

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -19,25 +19,46 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import ListCards from './ListCards/ListCards'
 import { mapOrder } from '~/utils/sorts'
 
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
 
 function Column({ column }) {
+  /*Keo tha */
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: column._id,
+    data: { ...column }
+  })
+  const dndKitColumnStyle = {
+    // touchAction: 'none',
+    //Nếu sử dụng transform như Doc sẽ bị lỗi strech nên chuyển sang translate
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
+  /*DropDown Menu */
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => { setAnchorEl(event.currentTarget) }
   const handleClose = () => { setAnchorEl(null) }
-
+  /*Sap xep card */
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
 
   return (
-    <Box sx={{
-      minWidth: '300px',
-      maxWidth: '300px',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
-      ml: 2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-    }}>
+    <Box
+      ref={setNodeRef}
+      style={dndKitColumnStyle}
+      {...attributes}
+      {...listeners}
+      sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }}>
       {/*Box Column Header */}
       <Box sx={{
         height: (theme) => {theme.trello.columnHeaderHeight},
@@ -53,7 +74,7 @@ function Column({ column }) {
             fontWeight: 'bold',
             cursor: 'pointer'
           }}>
-            {column?.title}
+          {column?.title}
         </Typography>
         <Box>
           <Tooltip title="More option">
