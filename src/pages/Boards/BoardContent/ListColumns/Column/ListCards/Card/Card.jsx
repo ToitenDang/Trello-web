cc57269319b1d11b51e76f2989e0ba433bf74728
ToitenDang <tkdang21@gmail.com>
2024-06-08 13:11:31 +0700

    Handle Drag Over drag_drop_crard_p2

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -22,7 +22,8 @@ function Card({ card }) {
     //Nếu sử dụng transform như Doc sẽ bị lỗi strech nên chuyển sang translate
     transform: CSS.Translate.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : undefined
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '1px solid #2ecc71' : undefined
   }
   const shouldShowCardActions = () => {
     return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
